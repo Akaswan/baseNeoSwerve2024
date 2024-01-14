131d742022ed4850cd6f4f86dd580a9254cbd9bd
Akaswan <akaswan06@gmail.com>
2024-01-14 09:28:07 -0600

    Update buildconstants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "baseNeoSwerve2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 80;
-  public static final String GIT_SHA = "f4cece41b4eaae4e1feba001524bb263d4a9cb87";
-  public static final String GIT_DATE = "2024-01-14 00:47:40 EST";
+  public static final int GIT_REVISION = 81;
+  public static final String GIT_SHA = "fe6f3ce18c79656a60b475d6a14267d934bbe18f";
+  public static final String GIT_DATE = "2024-01-14 01:52:07 EST";
   public static final String GIT_BRANCH = "HermesBaseFalconSwerve";
-  public static final String BUILD_DATE = "2024-01-14 01:51:44 EST";
-  public static final long BUILD_UNIX_TIME = 1705215104148L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-01-14 09:45:58 EST";
+  public static final long BUILD_UNIX_TIME = 1705243558247L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
