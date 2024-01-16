4ff5f47a29e22e6bd9d7f4276547fcd3c71bf4e1
Akaswan <akaswan06@gmail.com>
2024-01-15 18:08:51 -0600

    Hardware sim issue is m_pigeon.setYaw

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "baseNeoSwerve2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 79;
-  public static final String GIT_SHA = "5eb10341a0b1c4af59dceaa4e7dfd86f47e04aad";
-  public static final String GIT_DATE = "2024-01-14 15:42:37 EST";
+  public static final int GIT_REVISION = 81;
+  public static final String GIT_SHA = "91ebc89085927945ad260d79e4d0e6d70d8848d7";
+  public static final String GIT_DATE = "2024-01-14 17:49:35 EST";
   public static final String GIT_BRANCH = "Crescendo-Main";
-  public static final String BUILD_DATE = "2024-01-14 17:44:38 EST";
-  public static final long BUILD_UNIX_TIME = 1705272278568L;
+  public static final String BUILD_DATE = "2024-01-15 19:08:19 EST";
+  public static final long BUILD_UNIX_TIME = 1705363699145L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
